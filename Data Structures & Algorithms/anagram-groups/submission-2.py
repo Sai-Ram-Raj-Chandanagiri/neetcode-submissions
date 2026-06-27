32899d3b2e7872c2e97b7d5225b73d63191e61ca
Sai_Ram_Raj_Chandanagiri <164074051+Sai-Ram-Raj-Chandanagiri@users.noreply.github.com>
2026-06-28 00:40:02 +0530

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.py
@@ -0,0 +1,26 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        '''Approach Convert all of the strings into frequency dictionaries
+        and Keep track of them using two lists as dicts cannot be keys of dicts'''
+        group_list_1=[]
+        group_dict = {}
+        group_list_2=[]
+        def count_dict(s:str):
+            d={}
+            for i in range(len(s)):
+                if s[i] in d:
+                    d[s[i]]+=1
+                else:
+                    d[s[i]]=1
+            return d
+        for i in range(len(strs)):
+            a= count_dict(strs[i])
+            if a not in group_list_1:
+                group_list_1.append(a)
+                group_dict[len(group_list_1)]=a
+                group_list_2.append([strs[i]])
+            else:
+                for j in group_dict:
+                    if group_dict[j] == a:
+                        group_list_2[j-1]+=[strs[i]]
+        return group_list_2
